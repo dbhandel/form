531af8b4f0a088c23e2b15419264a16cb829cebe
David Handel <handeld@gmail.com>
2013-05-14 16:16:29 -0700

    partially completed validation with bugs in email validation

--- form.js
+++ form.js
@@ -1,43 +1,97 @@
 $(function() {
 	$('form').submit(function(e) {
-		e.preventDefault()
-  		console.log('Handler for .submit() called.');
-  		validateForm()	//need to pass in the contents (values) of all 4 fields as parameters??
+		e.preventDefault();
+  		validateForm();	
+
 	}
 	);
 	 
 }    
 )  
 	
-
 function validateForm () {
-	validateName()
-	validateEmail()
-	validateZip()
-	validatePhone()
+	//pass in the contents (values) of all 4 fields as parameters
+	var 	alert = 0,  //flag that form is valid
+		nameValue =   $('#name').val(),   
+		emailValue =   $('#email').val(),
+		zipValue =       $('#zipCode').val(),
+		phoneValue =  $('#phone').val() ; 
+		console.log(nameValue);
+	
+	validateName(nameValue) ;
+	checkAlert()
+	
+	validateEmail(emailValue);
+	/*validateZip(zipValue);
+	validatePhone(phoneValue);}*/
+	 // console.log("alert was set to: " + alert);
+	 function checkAlert() {
+	 	if (alert !== 0) {
+		$('body').append('<p>This form is invalid!</p>');
+		return;
+	}
+	 }
+
+	function validateName (nameValue) {
+	 	for (i =0; i < nameValue.length; i +=1) {
+	 		var utf8 = nameValue[i].charCodeAt();
+	 		console.log('the unicode for ' + nameValue[i] + " is " + utf8);
+	 		if (!(utf8 >64 && utf8 <123)) {
+	 			alert  = 1;  //not a letter
+	 			return
+	 		}
+ 		
+ 		}
+	}
+
+	function validateEmail (zipValue) {
+	console.log(zipValue);
+	var 	indexofAtSign = zipValue.indexof('@'),
+		lastDot = zipValue.lastindexof('.');  //in case there are mult dots and one comes before the '@'';
+
+	console.log('Inside validateEmail(). The index of the @ is: ' + indexofAtSign);
+	if (indexofAtSign > 0 && (lastDot > indexofAtSign)) {
+		var cc = charCodeAt();
+		if ( isChar(cc(lastDot-1))) {
+			for (i=1; i<4; i+=1) {
+				if ( isChar(cc(lastDot+i))) {
+					alert = 0;
+				}
+				else { 
+					alert = 1;
+					return 
+				}
+		return
+		}		
+	
+		alert = 1;
+		return  
+	}
+		 
+	}
+	alert = 1;
+	return;	
+	
+	function isChar (cc) {
+		if((cc>47 && cc<58) || (cc>64 && cc<91) || (cc>96 && cc<123)) {
+			return true
+		}
+		return false;
+	}
+}
 }
 
-//validate name field to have only letters
-function validateName (argument) {
-	// validate Name then deliver alert message if needed
-}
 
-/*validate Email address: includes @, a dot, at least 1 character before the dot, and 
-		at least 3 characters after the dot.  indexof */
-function validateEmail (argument) {
-	// validate email then deliver alert message if needed
-}
-
-// validate Zip: 5 digits
+/*// validate Zip: 5 digits
 function validateZip (argument) {
 	//  validate zip code then deliver alert message if needed.
-}
+
 
 // validate Phone: 10 digits, with or without dashes.
 function validatePhone (argument) {
 	// validate phone then deliver alert message if needed
-}	
-
+	
+*/
 
 /*var $form = $('form');
 	// listen for click on submit or enter key keyup
