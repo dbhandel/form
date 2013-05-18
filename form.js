d34eba94c5aa77892e19b7181d730d129318fa69
David Handel <handeld@gmail.com>
2013-05-18 10:05:36 -0400

    Name and Email reuired. Zip and Phone optional.

--- form.js
+++ form.js
@@ -2,9 +2,7 @@ $(function() {
 	$('form').submit(function(e) {
 		e.preventDefault();
   		validateForm();	
-
-	});
-	 
+	});	 
 });  
 	
 function validateForm () {
@@ -15,14 +13,21 @@ function validateForm () {
 		zipValue =       $('#zipCode').val(),
 		phoneValue =  $('#phone').val() ; 
 	
+	
 	validateName(nameValue) ;
-	checkAlert()
+	checkAlert();
 	
 	validateEmail(emailValue);
-	checkAlert()
+	checkAlert();
 
-	/*validateZip(zipValue);
-	validatePhone(phoneValue);}*/
+	if (zipValue != ''){ 
+		validateZip (zipValue);
+		checkAlert();
+	}
+	if (phoneValue != ''){
+		validatePhone(phoneValue);
+		checkAlert();
+	}
 
 	 function checkAlert() {
 	 	if (alert !== 0) {
@@ -41,16 +46,16 @@ function validateForm () {
  		}
 	}
 
-	function validateEmail (zipValue) {
-		var 	indexofAtSign = zipValue.indexOf('@'),
-			lastDot = zipValue.lastIndexOf('.');  //in case of mult dots and one comes before the '@'';
+	function validateEmail (emailValue) {
+		var 	indexofAtSign = emailValue.indexOf('@'),
+			lastDot = emailValue.lastIndexOf('.');  //in case of mult dots and one comes before the '@'';
 		if (
 			indexofAtSign > 0
 			&& lastDot > indexofAtSign 
-			&& isChar(zipValue.charCodeAt(lastDot - 1))
-			&& isChar(zipValue.charCodeAt(lastDot + 1))
-			&& isChar(zipValue.charCodeAt(lastDot + 2)) 
-			&& isChar(zipValue.charCodeAt(lastDot + 3)) 
+			&& isChar(emailValue.charCodeAt(lastDot - 1))
+			&& isChar(emailValue.charCodeAt(lastDot + 1))
+			&& isChar(emailValue.charCodeAt(lastDot + 2)) 
+			&& isChar(emailValue.charCodeAt(lastDot + 3)) 
 			) {
 			return alert = 0;
 		}
@@ -66,19 +71,74 @@ function validateForm () {
 		}
 		return false;
 	}
+
+	// validate Zip: 5 digits
+	function validateZip (zipValue) {
+		
+		for (i=0; i < 5; i++) {
+			 if ( (zipValue.charCodeAt(i) > 47  && zipValue.charCodeAt(i) < 58)) {
+			 }
+			 else {
+			 	return alert =1;
+			 }
+		}
+	}
+
+// validate Phone: 10 digits, with or without dashes.
+function validatePhone (phoneValue) {
+	for (i=0; i < 3; i++) {    //check that 1st three are digits
+		 if ( (phoneValue.charCodeAt(i) > 47  && phoneValue.charCodeAt(i) < 58)) {
+		 }
+		 else {
+		 	return alert =1;
+		 }
+	}
+	if ( (phoneValue.charCodeAt(3) > 47  && phoneValue.charCodeAt(3) < 58)) {
+		//if 4th is a digit check if the next six are digits (haven't yet prevented an 11th)
+		for (i=4; i < 10; i++) {
+			 if ( (phoneValue.charCodeAt(i) > 47  && phoneValue.charCodeAt(i) < 58)) {
+			 }
+			 else {
+			 	return alert =1;
+			 }
+		 return;
+		 }
+	} 
+	else {
+		if (phoneValue.charCodeAt(3) == 45) {
+			// if the 4th is a '-', check if the next three are digits
+			for (i=4; i < 7; i++) {
+			 	if ( (phoneValue.charCodeAt(i) > 47  && phoneValue.charCodeAt(i) < 58)) {
+			 	}
+			 	else {
+			 	return alert =1;
+			 	}
+		 	}
+			 if (phoneValue.charCodeAt(7) == 45)	{
+			 	// if the 7th is a '-', check if the next four are digits
+			 	for (i=8; i < 11; i++) {
+				 	if ( (phoneValue.charCodeAt(i) > 47  && phoneValue.charCodeAt(i) < 58)) {
+				 	}
+					else {
+					 	return alert =1;
+					 	}
+			 	}
+  			}
+		}
+		else {
+			return alert =1;	
+		}
+	}
+
+}
 }
 
 
 
-/*// validate Zip: 5 digits
-function validateZip (argument) {
-	//  validate zip code then deliver alert message if needed.
 
 
-// validate Phone: 10 digits, with or without dashes.
-function validatePhone (argument) {
-	// validate phone then deliver alert message if needed
+
 	
-*/
+
 
 
