3feaff07aae73bda0459b8c8526c5142d5fb701a
David Handel <handeld@gmail.com>
2013-05-19 15:29:10 -0400

    refactor with mult suggs by Raine and mult fn's moved out of validateForm.

--- form.js
+++ form.js
@@ -1,6 +1,7 @@
 $(function() {
 	$('form').submit(function(e) {
 		e.preventDefault();
+		$('p').empty();  //clears any previous "this form is invalid" message
   		validateForm();	
 	});	 
 });  
@@ -12,128 +13,120 @@ function validateForm () {
 		emailValue =   $('#email').val(),
 		zipValue =       $('#zipCode').val(),
 		phoneValue =  $('#phone').val() ; 
+		
+	var nameResult = validateName(nameValue) ;
+	isFormValid(nameResult);
 	
-	
-	validateName(nameValue) ;
-	checkAlert();
-	
-	validateEmail(emailValue);
-	checkAlert();
+	var emailResult = validateEmail(emailValue);
+	isFormValid(emailResult);
 
 	if (zipValue != ''){ 
-		validateZip (zipValue);
-		checkAlert();
+		var zipResult = validateZip (zipValue);
+		isFormValid(zipResult);
 	}
 	if (phoneValue != ''){
-		validatePhone(phoneValue);
-		checkAlert();
+		var phoneResult = validatePhone(phoneValue);
+		isFormValid(phoneResult);
 	}
 
-	 function checkAlert() {
-	 	if (alert !== 0) {
-		$('body').append('<p>This form is invalid!</p>');
-		return;
+		
+}
+ 
+function isFormValid(result) {
+		if (result === false) {
+	$('body').append('<p>This form is invalid!</p>');
+	return;
 	}
- }
+}
 
-	function validateName (nameValue) {
+function validateName (nameValue) {
 	 	for (i =0; i < nameValue.length; i++) {
 	 		var utf8 = nameValue[i].charCodeAt();
-	 		if (!(utf8 >64 && utf8 <123)) {
-	 			alert  = 1;  //not a letter
-	 			return
+	 		if (!((utf8 > 64 && utf8 <91) 
+			|| (utf8 > 96 && utf8 < 123))) {
+	 			return false;
 	 		}		
  		}
 	}
 
-	function validateEmail (emailValue) {
-		var 	indexofAtSign = emailValue.indexOf('@'),
-			lastDot = emailValue.lastIndexOf('.');  //in case of mult dots and one comes before the '@'';
-		if (
-			indexofAtSign > 0
-			&& lastDot > indexofAtSign 
-			&& isChar(emailValue.charCodeAt(lastDot - 1))
-			&& isChar(emailValue.charCodeAt(lastDot + 1))
-			&& isChar(emailValue.charCodeAt(lastDot + 2)) 
-			&& isChar(emailValue.charCodeAt(lastDot + 3)) 
-			) {
-			return alert = 0;
-		}
-		else {
-			return alert = 1;
-		}
+function validateEmail (emailValue) {
+	var 	indexofAtSign = emailValue.indexOf('@'),
+		lastDot = emailValue.lastIndexOf('.');  //in case of mult dots and one comes before the '@'';
+	if (
+		indexofAtSign > 0
+		&& lastDot > indexofAtSign 
+		&& isChar(emailValue.charCodeAt(lastDot - 1))
+		&& isChar(emailValue.charCodeAt(lastDot + 1))
+		&& isChar(emailValue.charCodeAt(lastDot + 2)) 
+		&& isChar(emailValue.charCodeAt(lastDot + 3)) 
+		) {
+		return true;
 	}
-	
-	function isChar (charCode) {
-		if((charCode > 47 && charCode < 58) || (charCode > 64 && charCode <91) 
-			|| (charCode > 96 && charCode < 123)) {
-			return true
-		}
+	else {
 		return false;
 	}
-
-	// validate Zip: 5 digits
-	function validateZip (zipValue) {
-		
-		for (i=0; i < 5; i++) {
-			 if ( (zipValue.charCodeAt(i) > 47  && zipValue.charCodeAt(i) < 58)) {
-			 }
-			 else {
-			 	return alert =1;
-			 }
-		}
+}
+	
+function isChar (charCode) {
+	if((charCode > 47 && charCode < 58) || (charCode > 64 && charCode <91) 
+		|| (charCode > 96 && charCode < 123)) {
+		return true
 	}
+	return false;
+}
+
+// validate Zip: 5 digits
+function validateZip (zipValue) {
+	
+	for (i=0; i < 5; i++) {
+		 if ( (zipValue.charCodeAt(i) > 47  && zipValue.charCodeAt(i) < 58)) {
+		 }
+		 else {
+		 	return false;
+		 }
+	}
+}
 
 // validate Phone: 10 digits, with or without dashes.
 function validatePhone (phoneValue) {
 	for (i=0; i < 3; i++) {    //check that 1st three are digits
-		 if ( (phoneValue.charCodeAt(i) > 47  && phoneValue.charCodeAt(i) < 58)) {
-		 }
-		 else {
-		 	return alert =1;
+		 if (!(phoneValue.charCodeAt(i) > 47  && phoneValue.charCodeAt(i) < 58)) {
+		 	return false;
 		 }
 	}
 	if ( (phoneValue.charCodeAt(3) > 47  && phoneValue.charCodeAt(3) < 58)) {
-		//if 4th is a digit check if the next six are digits (haven't yet prevented an 11th)
+		console.log(phoneValue[3]);
+		//if 4th is a digit, check if the next six are digits (haven't yet prevented an 11th)
 		for (i=4; i < 10; i++) {
-			 if ( (phoneValue.charCodeAt(i) > 47  && phoneValue.charCodeAt(i) < 58)) {
+			 if (!(phoneValue.charCodeAt(i) > 47  && phoneValue.charCodeAt(i) < 58)) {
+			 	return false;
 			 }
-			 else {
-			 	return alert =1;
-			 }
-		 return;
 		 }
+	 return true;  //no further char testing needed, the entry proven to be 10 digits
 	} 
 	else {
 		if (phoneValue.charCodeAt(3) == 45) {
 			// if the 4th is a '-', check if the next three are digits
 			for (i=4; i < 7; i++) {
-			 	if ( (phoneValue.charCodeAt(i) > 47  && phoneValue.charCodeAt(i) < 58)) {
-			 	}
-			 	else {
-			 	return alert =1;
+			 	if (!(phoneValue.charCodeAt(i) > 47  && phoneValue.charCodeAt(i) < 58)) {
+			 	return false;
 			 	}
 		 	}
 			 if (phoneValue.charCodeAt(7) == 45)	{
 			 	// if the 7th is a '-', check if the next four are digits
 			 	for (i=8; i < 11; i++) {
-				 	if ( (phoneValue.charCodeAt(i) > 47  && phoneValue.charCodeAt(i) < 58)) {
-				 	}
-					else {
-					 	return alert =1;
+				 	if (!(phoneValue.charCodeAt(i) > 47  && phoneValue.charCodeAt(i) < 58)) {
+					 	return false;
 					 	}
 			 	}
   			}
 		}
 		else {
-			return alert =1;	
+			return false;	
 		}
 	}
 
 }
-}
-
-
 
 
 
